bb8616a5f06c75b5c1c750151a6214a683aefa62
MarianoOTech <marianosanro23@gmail.com>
2024-10-15 12:36:25 -0600

    branch a1

--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -17,13 +17,13 @@ describe('MediaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should calculate media for integer input', () => {
+  it('should return media = 550.6 for the first input', () => {
     const input = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
     const expectedMedia = 550.6;
     expect(component.calcularMedia(input)).toEqual(expectedMedia);
   });
   
-  it('should calculate media for float input', () => {
+  it('should return media = 60.32 for the second input', () => {
     const input = [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2];
     const expectedMedia = 60.32;
     expect(component.calcularMedia(input)).toEqual(expectedMedia);
